8cf0ca0b1476ec6bae25e9b7528ce9e04cf4d11d
Veronika Srenk <moormie@gmail.com>
2022-09-12 18:11:40 +0200

    added icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,7 @@
 export { Calendar } from "./CalendarIcon";
 export { CaretLeft } from "./CaretLeftIcon";
 export { CaretRight } from "./CaretRightIcon";
+export { Add } from "./AddIcon";
+export { BarChart } from "./BarChartIcon";
+export { Menu } from "./MenuIcon";
+export { Car } from "./CarIcon";
