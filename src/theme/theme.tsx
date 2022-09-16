1a67e94600361c53784160fbcc4a83b69104c4ab
Veronika Srenk <moormie@gmail.com>
2022-09-16 18:02:26 +0200

    add themes

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,9 +1,59 @@
 import { DefaultTheme } from "styled-components";
 
-export const myTheme: DefaultTheme = {
+interface Color {
+  grape: string;
+  red: string;
+  orange: string;
+  yellow: string;
+  yellowGreen: string;
+  green: string;
+  brown: string;
+}
+
+interface MyTheme extends DefaultTheme {
+  borderRadius: string;
+  colors: {
+    lightGray: string;
+    gray: string;
+    pastel: {};
+    grape: string;
+    red: string;
+    orange: string;
+    yellow: string;
+    yellowGreen: string;
+    green: string;
+    brown: string;
+    dark: Color;
+  };
+}
+
+export const myTheme: MyTheme = {
   borderRadius: "18px",
   colors: {
     lightGray: "#f2f2f2",
     gray: "#e0e0e0",
+    pastel: {
+      orange: "#ffc886",
+      pink: "#cc85aa",
+      green: "#8cd98c",
+      turquoise: "#60cfd1",
+      blue: "#7dd3fb",
+    },
+    grape: "#665075",
+    red: "#C75554",
+    orange: "#DF810B",
+    yellow: "#D9B35B",
+    yellowGreen: "#B2BC76",
+    green: "#778C62",
+    brown: "#71533d",
+    dark: {
+      grape: "#42314d",
+      red: "#903d3c",
+      orange: "#a4600d",
+      yellow: "#9f8039",
+      yellowGreen: "#8c974c",
+      green: "#566b40",
+      brown: "#3c2b1f",
+    },
   },
 };
