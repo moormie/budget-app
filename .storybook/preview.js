fb15d9a223090d36f9472cafcb7d41259c4d4096
Veronika Srenk <moormie@gmail.com>
2022-09-16 13:26:59 +0200

    added custom theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { myTheme } from "../src/theme";
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={myTheme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
---
+++ src/theme/theme.tsx
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+export const myTheme: DefaultTheme = {
+  borderRadius: "18px",
+  colors: {
+    lightGray: "#f2f2f2",
+    gray: "#e0e0e0",
+  },
+};
