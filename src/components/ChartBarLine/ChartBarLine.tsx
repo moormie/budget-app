cb17f422e57999616033ad600a86cb91540ddba8
Veronika Srenk <moormie@gmail.com>
2022-09-27 17:29:35 +0200

    BarChart animation

--- src/components/ChartBarLine/ChartBarLine.tsx
+++ src/components/ChartBarLine/ChartBarLine.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Container = styled.div`
   width: 10px;
@@ -11,11 +11,22 @@ const Container = styled.div`
   align-items: end;
   padding: 1px;
 `;
+
+const grow = (height: number) => keyframes`
+  0% {
+    height: 0%;
+  }
+  100% {
+    height: ${height}%;
+  }
+`;
+
 const Line = styled.div<{ height: number; color?: string }>`
   height: ${(props) => props.height}%;
   width: 100%;
   background-color: ${(props) => props?.color ?? props.theme.colors.gray};
   border-radius: 6px;
+  animation: ${(props) => grow(props.height)} 1s ease-out;
 `;
 
 interface ChartBarLine {
