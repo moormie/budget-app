7491dc5e2431062c5b89f5c2d89201918fce06cc
Veronika Srenk <moormie@gmail.com>
2022-09-30 19:15:42 +0200

    Category to enum

--- src/types/Category.ts
+++ src/types/Category.ts
@@ -1,8 +1,9 @@
-export type Category =
-  | "Food"
-  | "Home"
-  | "Car"
-  | "Entertainment"
-  | "Medical"
-  | "Personal"
-  | "Travel";
+export enum Category {
+  FOOD = "Food",
+  HOME = "Home",
+  CAR = "Car",
+  ENTERTAINMENT = "Entertainment",
+  MEDICAL = "Medical",
+  PERSONAL = "Personal",
+  TRAVEL = "Travel",
+}
--- src/types/mockData.ts
+++ src/types/mockData.ts
@@ -1,64 +1,65 @@
+import { Category } from "./Category";
 import { Expenses } from "./Expenses";
 import { Income } from "./Income";
 
 export const expensesMockData: Expenses[] = [
   {
     id: "001",
-    category: "Food",
+    category: Category.FOOD,
     amount: 110,
     date: "01-09-2022",
     note: "Some note...",
   },
   {
     id: "002",
-    category: "Car",
+    category: Category.CAR,
     amount: 60,
     date: "03-09-2022",
     note: "Some note...",
   },
   {
     id: "003",
-    category: "Medical",
+    category: Category.MEDICAL,
     amount: 120,
     date: "10-09-2022",
     note: "Some note...",
   },
   {
     id: "004",
-    category: "Food",
+    category: Category.FOOD,
     amount: 90,
     date: "14-09-2022",
     note: "Some note...",
   },
   {
     id: "005",
-    category: "Travel",
+    category: Category.TRAVEL,
     amount: 80,
     date: "19-09-2022",
     note: "Some note...",
   },
   {
     id: "006",
-    category: "Home",
+    category: Category.HOME,
     amount: 500,
     date: "19-09-2022",
     note: "Some note...",
   },
   {
     id: "007",
-    category: "Travel",
+    category: Category.TRAVEL,
     amount: 62,
     date: "20-09-2022",
   },
   {
     id: "008",
-    category: "Entertainment",
+    category: Category.ENTERTAINMENT,
     amount: 45,
     date: "20-09-2022",
   },
   {
     id: "009",
-    category: "Personal",
+    category: Category.PERSONAL,
     amount: 160,
     date: "20-09-2022",
   },
