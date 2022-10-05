58143637d9982ff7491a73f2cfc39bb70d6876a9
Veronika Srenk <moormie@gmail.com>
2022-10-05 17:25:21 +0200

    delete unused counter from store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import expensesReducer from "../features/expenses/expensesSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     expenses: expensesReducer,
   },
 });
