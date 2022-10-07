e856a2753235afd9e349523b687e4f86f1bd671c
Veronika Srenk <moormie@gmail.com>
2022-10-07 13:40:38 +0200

    create Alert modal

---
+++ src/components/Alert/Alert.stories.tsx
@@ -0,0 +1,35 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Button } from "../Button/Button";
+import { Alert } from "./Alert";
+import { useArgs } from "@storybook/client-api";
+
+export default {
+  title: "Components/Alert",
+  component: Alert,
+  parameters: {
+    layout: "fullscreen",
+  },
+  args: {
+    visible: false,
+  },
+} as ComponentMeta<typeof Alert>;
+
+const Template: ComponentStory<typeof Alert> = ({ ...args }) => {
+  const [{ visible }, updateArgs] = useArgs();
+
+  const handleClose = () => updateArgs({ visible: visible });
+
+  return (
+    <>
+      <div style={{ margin: 32 }}>
+        <Button
+          onClick={() => updateArgs({ visible: !visible })}
+          label="OPEN"
+        ></Button>
+      </div>
+      <Alert {...args} />
+    </>
+  );
+};
+
+export const Default = Template.bind({});
