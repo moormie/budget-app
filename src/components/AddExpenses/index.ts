25b23fdacf1e5950acd447d32d4d9acd2292470a
Veronika Srenk <moormie@gmail.com>
2022-10-13 17:04:35 +0200

    update AddExpenses UI

---
+++ src/components/AddExpenses/index.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Title = styled.div`
+  text-align: center;
+  padding: 18px;
+`;
+
+const Spacing = styled.div`
+  margin-bottom: 18px;
+`;
+
+const ButtonContainer = styled.div`
+  padding: 48px 0;
+`;
+
+const StyledModal = {
+  Title,
+  Spacing,
+  ButtonContainer,
+};
+
+export default StyledModal;
